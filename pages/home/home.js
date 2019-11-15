152778df1a4cbaa5b6188b3a9c55267f0ec57e99
jinmu.zhou <870855698@qq.com>
2019-11-15 15:48:31 +0800

    组件使用及样式

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,7 +3,9 @@
 const app = getApp()
 console.log(app.globalData.name)
 console.log(app.globalData.age)
+
 Page({
+  //2.初始化数据
   data: {
     name: "普罗米修斯",
     students: [
@@ -11,40 +13,86 @@ Page({
       { id: 2, name: "关羽", age: 56 },
       { id: 3, name: "张飞", age: 56 },
       { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
+      { id: 4, name: "诸葛亮", age: 56 },
       { id: 5, name: "赵云", age: 56 }
     ],
     count: 0,
     list: []
   },
+ 
+  //1.监听页面周期函数 
+  onLoad() {
+    console.log('onLoad');
+  },
+  onShow() {
+    console.log('onShow')
+    //函数内不能直接用this，要在外面定义
+    var that = this
+    wx.request({
+      url: ' http://123.207.32.32:8000/recommend',
+      success: function(res) {
+        console.log(res)
+        const dd = res.data;
+        that.setData({
+          list: dd
+        })
+      }
+      //箭头函数可以用this，
+      // success: (res) => {
+        // this.data
+      // }
+    })
+  },
+  //初次渲染完成显示
+  onReady() {
+    console.log('onReady')
+  },  
+  //页面隐藏时执行
+  onHide() {
+    console.log('onHide')
+  }, 
+  onUnload() {
+    console.log('onUnLoad')
+  },
+
+
+//3.监听页面内的事件
   addCount() {
     this.setData({
-      count: this.data.count+1
+      count: this.data.count + 1
     })
   },
   getUserInfo(event) {
     console.log("用户信息")
     console.log(event)
   },
-  onShow() {
-    console.log('onShow')
-    wx.request({
-      url: 'http://123.207.32.32:8000/recommend',
-      success: function(res) {
-        console.log(res)
-        const dd = res.data;
-        this.setData({
-          list: dd
-        })
-      }
-    })
+  handleViewClick() {
+    console.log('该文字被点击了')
   },
-  onReady() {
-    console.log('onReady')
-  },  
-  onHide() {
-    console.log('onHide')
+
+  //4.监听其他事件
+  //监听页面滚动
+  onPageScroll(obj) {
+    // console.log(obj)
   },
-  onUnload() {
-    console.log('onUnLoad')
+  //监听页面滚动到底部
+  onReachBottom() {
+    console.log('页面滚动到底部')
   }
 })
